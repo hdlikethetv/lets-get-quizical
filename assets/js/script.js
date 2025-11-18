b625002076c0a05bd886b57eea99dcf4a9f08fb5
decccmcc <decccmcc@gmail.com>
2025-11-18 14:59:32 +0000

    Add basic start quiz function and start quiz button event listener

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,26 @@
 // Get references to the different screens
-
 const startScreen = document.getElementById('start-screen');
 const quizScreen = document.getElementById('quiz-screen');
 const resultScreen = document.getElementById('results-screen');
 
+// Get start page element references
+const startButton = document.getElementById('start-btn');
+const usernameInput = document.getElementById('username-input');
+
+
+// Get quiz page element references
+const usernameDisplay = document.getElementById('display-username');
+
+// Event Listeners
+startButton.addEventListener('click', startQuiz);
+
 // Function to show a specific screen and hide the others
 function showScreen(screen) {
     startScreen.classList.add('hidden');
-    quizScreen.classList.add('hidden');
+    quizScreen.classList.add('hidden');     // Hide alll
     resultScreen.classList.add('hidden');
 
+    // Show the selected screen
     if (screen === 'start') {
         startScreen.classList.remove('hidden');
     } else if (screen === 'quiz') {
@@ -20,3 +31,21 @@ function showScreen(screen) {
 }
 
 showScreen('start'); // Show the start screen by default
+
+function startQuiz() {
+    const enteredName = usernameInput.value.trim(); // Remove whitespace and set name
+    if (!enteredName) {
+        alert('Please enter your name to start the quiz.');
+        return;
+    }
+
+    if (enteredName.length < 3) {
+        alert('Name must be at least 3 characters long.');
+        return;
+    }
+
+    
+    usernameDisplay.textContent = enteredName;
+    showScreen('quiz');
+}
+
